0d6a1ce7c454278f81594d1d0ecb41ef8e70fdfd
Nick-Lemy <n.kayiranga@alustudent.com>
2025-09-01 07:40:14 +0200

    feat: add initial project data for display in the application

---
+++ src/utils/dummydata.ts
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    id: 1,
+    titre:
+      "Travaux de second œuvre (électricité et enduit au mortier de ciment) - Bâtiment R+4",
+    lieu: "Bonamoussadi/Denver, Douala",
+    catégorie: "Second Œuvre",
+    description:
+      "Installations électriques et enduit au mortier de ciment pour un bâtiment de 4 étages.",
+    imageDeCouverture:
+      "https://example.com/images/second-oeuvre-couverture.jpg",
+    autresImages: [],
+  },
+  {
+    id: 2,
+    titre: "Installation d'un système d'alimentation avec nourrice pour douche",
+    lieu: "Sawa, Douala",
+    catégorie: "Plomberie",
+    description:
+      "Installation d'un système d'alimentation avec nourrice pour une douche.",
+    imageDeCouverture: "https://example.com/images/plomberie-couverture.jpg",
+    autresImages: [],
+  },
+  {
+    id: 3,
+    titre: "Rénovation d'un bâtiment R+3 à usage d'habitation",
+    lieu: "Logbessou, Douala",
+    catégorie: "Rénovation",
+    description:
+      "Travaux de rénovation pour un bâtiment résidentiel de 3 étages.",
+    imageDeCouverture: "https://example.com/images/renovation-couverture.jpg",
+    autresImages: [],
+  },
+  {
+    id: 4,
+    titre: "Essais de pénétromètre en collaboration avec Geo Fondation",
+    lieu: "Ambassade de France, Yaoundé",
+    catégorie: "Tests Géotechniques",
+    description:
+      "Essais de pénétration réalisés en collaboration avec Geo Fondation à l'Ambassade de France.",
+    imageDeCouverture: "https://example.com/images/geotechnique-couverture.jpg",
+    autresImages: [],
+  },
+];

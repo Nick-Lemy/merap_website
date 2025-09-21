4b1d2f304da179db264b36f044c1c658f8e6db99
Nick-Lemy <n.kayiranga@alustudent.com>
2025-09-21 13:24:15 +0200

    feat(ServicesPage, ServiceDetailsPage): implement service details page and update services data structure

--- src/app/(home)/services/page.tsx
+++ src/app/(home)/services/page.tsx
@@ -2,17 +2,9 @@
 
 import React from "react";
 import { motion } from "framer-motion";
-import {
-  Building,
-  Construction,
-  Wrench,
-  Palette,
-  Droplets,
-  Zap,
-} from "lucide-react";
-import Image, { StaticImageData } from "next/image";
-import RenovationImage from "@/assets/renovation.png";
-import Etude from "@/assets/etude.jpg";
+import Image from "next/image";
+import Link from "next/link";
+import { services, Service } from "@/utils/servicesData";
 
 export default function ServicesPage() {
   return (
@@ -71,63 +63,6 @@ function HeroSection() {
 }
 
 function ServicesGrid() {
-  const services = [
-    {
-      image:
-        "https://i.pinimg.com/1200x/42/28/24/42282462f38c8cae0035f69733a9ef7f.jpg",
-      icon: Building,
-      title: "Construction de Bâtiments",
-      description:
-        "Gros œuvre et second œuvre pour bâtiments à usage d'habitation et commercial avec des matériaux de qualité.",
-    },
-    {
-      image: Etude,
-      icon: null,
-      title: "Études et Gestion de Projets",
-      description:
-        "Conception, planification et gestion complète de vos projets de construction avec un suivi rigoureux Du debut jusqu'a la fin.",
-    },
-    {
-      image: RenovationImage,
-      icon: Wrench,
-      title: "Rénovations et Réhabilitation",
-      description:
-        "Travaux de rénovation, réhabilitation et aménagement intérieur (peinture, carrelage, Pierre de Pouma).",
-    },
-    {
-      image:
-        "https://img.freepik.com/photos-premium/pelle-niveleuse-se-tiennent-cote-cote-contre-ciel-bleu-engins-terrassement-construction-lourde-construction-routes-communications-souterraines-industrie-construction_331695-519.jpg",
-      icon: Construction,
-      title: "Construction de Routes",
-      description:
-        "Terrassement et revêtement routier avec expertise technique pour des infrastructures durables.",
-    },
-    {
-      image:
-        "https://comparateurtravaux.fr/wp-content/uploads/2017/10/cr%C3%A9pis-8.jpg",
-      icon: Palette,
-      title: "Travaux de Finitions",
-      description:
-        "Finitions de haute qualité pour donner à vos projets l'aspect final souhaité.",
-    },
-    {
-      image:
-        "https://img.freepik.com/premium-photo/closeup-african-plumber-showing-one-dirty-one-clean-filters-changing-water-filters_404612-2338.jpg",
-      icon: Droplets,
-      title: "Installation Sanitaire",
-      description:
-        "Conception et installation de systèmes de plomberie conformes aux normes en vigueur.",
-    },
-    {
-      image:
-        "https://i.pinimg.com/736x/20/f5/21/20f521248c2b9d07de1b59932871df30.jpg",
-      icon: Zap,
-      title: "Électricité",
-      description:
-        "Conception et installation électrique sécurisées et aux normes.",
-    },
-  ];
-
   return (
     <section className="py-20 lg:py-32 bg-gray-50">
       <div className="container mx-auto px-6 lg:px-20">
@@ -150,18 +85,12 @@ function ServicesGrid() {
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {services.map((service, index) => (
-            <ServiceCard key={index} service={service} index={index} />
+            <ServiceCard key={service.id} service={service} index={index} />
           ))}
         </div>
       </div>
     </section>
   );
-}
-
-interface Service {
-  image: string | StaticImageData;
-  title: string;
-  description: string;
 }
 
 function ServiceCard({ service, index }: { service: Service; index: number }) {
@@ -190,13 +119,15 @@ function ServiceCard({ service, index }: { service: Service; index: number }) {
           {service.title}
         </h3>
         <p className="text-gray-200 leading-relaxed">{service.description}</p>
-        <motion.button
-          className="mt-6 cursor-pointer text-[var(--color-secondary)] font-semibold hover:text-[var(--color-primary)] transition-colors duration-300"
-          whileHover={{ x: 5 }}
-          transition={{ duration: 0.2 }}
-        >
-          En savoir plus →
-        </motion.button>
+        <Link href={`/services/${service.id}`}>
+          <motion.button
+            className="mt-6 cursor-pointer text-[var(--color-secondary)] font-semibold hover:text-[var(--color-primary)] transition-colors duration-300"
+            whileHover={{ x: 5 }}
+            transition={{ duration: 0.2 }}
+          >
+            En savoir plus →
+          </motion.button>
+        </Link>
       </div>
     </motion.div>
   );
